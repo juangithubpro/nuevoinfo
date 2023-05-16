6c602556ee31c32a7a99035f214fb5eebe201bf9
juangithubpro <juanchitorrinco@gmail.com>
2023-05-16 11:23:21 -0300

    ejercicios4

---
+++ semana4/ejercicios4.py
@@ -0,0 +1,20 @@
+# ejercicios 4
+
+""" def saludar(nombre):
+    print("hola,", nombre)
+
+saludar("juan") """ 
+
+# ejercicio 1
+
+"""
+
+def suma(numero1,numero2):
+    print(numero1 + numero2)
+
+
+
+suma(1459,5679) 
+
+"""
+
