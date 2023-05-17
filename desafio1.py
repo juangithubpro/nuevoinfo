918a1a34d91872961a791926e8e60ce5461ad886
juangithubpro <juanchitorrinco@gmail.com>
2023-05-16 22:07:57 -0300

    deafio1

---
+++ desafio1.py
@@ -0,0 +1,26 @@
+# desafio 1
+
+# nombre = input("ingrese su nombre: ")
+# ventas = float(input("ingrese cuanto vendio"))
+# comision = ventas * 0.06
+# print(f"{nombre}, recibira una comision {comision:.2f} por sus ventas")
+
+
+# desafio 2
+
+nombre = input("ingrese su nombre: ")
+edad = input("ingrese su edad: ")
+altura = input("ingrese su altura: ")
+peso = float(input("ingrese su peso: "))
+direccion = input("ingrese su direccion: ")
+telefono = input("ingrese su telefono: ")
+
+print("La" + " " + "información" + " " + "ingresada" + " " + " " + "es" + " " +"la" + " " + "siguiente:"
+ )
+print("Nombre completo: " + (nombre))
+print("edad: " + str(edad))
+print("altura: " + str(altura))
+print("peso: " + str(peso) + ("kg"))
+print("direccion: " + str(direccion))
+print("telefono: " + (telefono))
+
